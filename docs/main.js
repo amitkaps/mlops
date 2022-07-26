67fa3576d80cdabe109c3981d12f844698c1ab2b
Amit Kapoor <amitkaps@gmail.com>
2022-07-26 16:22:16 +0530

    Fixed the docs build
    Preview changes for main.js was causing build failure. Removed changes to fix the build.

--- docs/main.js
+++ docs/main.js
@@ -1,17 +1,17 @@
 console.log("Running JS Code");
 
 /* For Dev Server */
-const routesList = import.meta.glob("./pages/*.md", { as: "raw" });
-const routes = Object.keys(routesList);
-console.log(routes);
+// const routesList = import.meta.glob("./pages/*.md", { as: "raw" });
+// const routes = Object.keys(routesList);
+// console.log(routes);
 
-const page = import.meta.glob(`./pages/*.md"`);
+// const page = import.meta.glob(`./pages/*.md"`);
 
-console.log(pages);
+// console.log(pages);
 
-let content = JSON.parse(pages["./pages/index.md"]);
+// let content = JSON.parse(pages["./pages/index.md"]);
 
-console.log(content);
+// console.log(content);
 
-const el = document.querySelector("#content");
-el.innerHTML = content;
+// const el = document.querySelector("#content");
+// el.innerHTML = content;
